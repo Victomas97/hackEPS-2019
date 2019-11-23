0bd14862051c34035d5cc8c84328cf52f348a5c0
Victor <victomas@outlook.es>
2019-11-23 21:01:34 +0100

    d

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var express        =         require("express");
-var bodyParser     =         require("body-parser");
-var app            =         express();
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.get('/',function(req,res){
 });
