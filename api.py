a77650d138e8db6eee85173c9dff07ed019bb38b
Victomas97 <victomas@outlook.es>
2019-11-24 00:03:40 +0100

    port 8080

--- api.py
+++ api.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
-cors = CORS(app, resources={r"/app/*": {"origins": "*"}})
 
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 class Hello(Resource):
