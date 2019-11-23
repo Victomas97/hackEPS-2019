ddc65419d9aae78055ddfc3fa408c0c5865f1389
Victor <victomas@outlook.es>
2019-11-23 21:03:37 +0100

    d

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var app = express();
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json())
+app.use (express.urlencoded({extended: false}))
+
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
-app.get('/',function(req,res){
-});
-
-app.post('/temp',function(req,res){
+app.post('/temp', (req, res) => {
     console.log(req.body);
-    res.end("yes");
 });
 
-app.listen(8080,function(){
-    console.log("Started on PORT 8080");
-})
+app.listen(PORT, () => console.log('Listening on ${ PORT }'));
