897cd010e832aedc34cadb0337a89d0ca4cb3ef6
Victor <victomas@outlook.es>
2019-11-23 21:04:15 +0100

    d

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json())
 app.use (express.urlencoded({extended: false}))
@@ -16,4 +16,6 @@ app.post('/temp', (req, res) => {
     console.log(req.body);
 });
 
-app.listen(PORT, () => console.log('Listening on ${ PORT }'));
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!');
+});
